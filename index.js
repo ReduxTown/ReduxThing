f85143b7ee587b0cbe3ec081c4ed531e77d518d2
Redux <katysalt.40@gmail.com>
2026-02-06 16:15:41 -0500

    Add terminal script functionality to index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log("GithubThing loaded!");
-console.log("You can run scripts from this terminal!");
+#!/usr/bin/env node
+
+console.log("ReduxThing Terminal Script is running!");
+
+const args = process.argv.slice(2);
+console.log("Arguments:", args);
+
+if (args.length === 0) {
+  console.log("No arguments passed. Try: node index.js hello");
+} else {
+  console.log("You ran:", args.join(" "));
+}
